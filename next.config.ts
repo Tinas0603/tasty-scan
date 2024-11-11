046412c8cd635713c1372131c3d5312c35625d4c
Tinas0603 <phamvutien03@gmail.com>
2024-11-11 08:53:37 +0700

    UI menu goi mon

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         hostname: 'localhost',
         pathname: '/**'
       },
+      {
+        hostname: 'via.placeholder.com',
+        pathname: '/**'
+      },
     ]
   }
 };
